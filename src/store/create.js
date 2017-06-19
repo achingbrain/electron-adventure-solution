58124211be6920e8ee54a149f52dd562956f3102
achingbrain <alex@achingbrain.net>
2017-06-19 12:32:57 +0100

    Remove more unnecessary deps

--- src/store/create.js
+++ src/store/create.js
@@ -1,35 +1,24 @@
 import { createStore, applyMiddleware, compose } from 'redux'
-import thunk from 'redux-thunk'
 import { createLogger } from 'redux-logger'
-import {persistStore, autoRehydrate} from 'redux-persist'
+import { persistStore, autoRehydrate } from 'redux-persist'
 import localForage from 'localforage'
 import rootReducer from '../reducers'
 
 const configureStore = (initialState) => {
   // Redux Configuration
-  const middleware = []
-  const enhancers = []
-
-  // Thunk Middleware
-  middleware.push(thunk)
-
-  // Logging Middleware
-  middleware.push(createLogger({
-    level: 'info',
-    collapsed: true
-  }))
-
-  enhancers.push(applyMiddleware(...middleware))
-  enhancers.push(autoRehydrate())
+  const middleware = [
+    createLogger({
+      level: 'info',
+      collapsed: true
+    })
+  ]
+  const enhancers = [
+    applyMiddleware(...middleware),
+    autoRehydrate()
+  ]
 
   // Create Store
   const store = createStore(rootReducer, initialState, compose(...enhancers))
-
-  if (module.hot) {
-    module.hot.accept(() =>
-      store.replaceReducer(require('../reducers').default) // eslint-disable-line global-require
-    )
-  }
 
   // begin periodically persisting the store
   persistStore(store, {storage: localForage})
